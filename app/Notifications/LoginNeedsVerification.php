d033dbaa5929fd3bb284f9c954fe60e69e12ad22
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-04-23 20:02:16 +0100

    added twilio

--- app/Notifications/LoginNeedsVerification.php
+++ app/Notifications/LoginNeedsVerification.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioSmsMessage;
 
 class LoginNeedsVerification extends Notification
 {
@@ -26,19 +27,24 @@ class LoginNeedsVerification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['TwilioChannel::class'];
     }
 
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
+   public function toTwilio($notifiable) {
+
+        $loginCode = rand(111111, 999999);
+
+        $notifiable->update([
+            'login_code' => $loginCode
+        ]);
+
+        return(new TwilioSmsMessage())
+        ->content("Your login was complete {$loginCode} do not share this with anybody");
+
+   }
 
     /**
      * Get the array representation of the notification.
