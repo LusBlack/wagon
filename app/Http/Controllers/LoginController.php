f0be8ddea43b8b330ba62f0e57d6a4222c938463
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-04-24 16:56:04 +0100

    added verify function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,5 +26,41 @@ class LoginController extends Controller
         }
 
         $user->notify(new LoginNeedsVerification());
+
+        // return a response
+        return response()->json(['message' => 'Text message notification sent. ']);
     }
+
+    public function verify(Request $request) {
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        //find the user
+        $user = User::where('phone', $request->phone)
+
+        ->where('login_code', $request->login_code)
+        ->first();
+
+        if($user) {
+            $user->update([
+                'login_code' => null
+
+            ]);
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+        if(!$user) {
+            return response()->json([
+             'message' => 'Invalid verification code.'
+            ], 401);
+        }
+        //send the user a one time use code
+        $user->notify(new LoginNeedsVerification());
+
+        //return response
+        return response()->json(['message' => 'Text message notification sent']);
+
+    }
+
 }
