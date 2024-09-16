66c868408d4e1aedad8014c2b1356af67339d224
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-09-16 17:48:23 +0100

    added route for trips

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DriverController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
@@ -22,6 +23,8 @@ Route::post('/login/verify', [LoginController::class, 'verify']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/driver', [DriverController::class, 'show']);
     Route::post('/driver', [DriverController::class, 'update']);
+
+    Route::post('/trip', [TripController::class, 'store']);
 
     Route::get('/user', function(Request $request) {
     return $request->user();
